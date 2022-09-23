85521df408eae22a7266e6cd3c7c05b2c7b73e17
Dong-Yeop <ehdduq53@gmail.com>
2022-09-23 17:06:32 +0900

    gsap v.1.0 적용 완료

--- src/components/ProjectVisual.vue
+++ src/components/ProjectVisual.vue
@@ -10,7 +10,7 @@
       src="@/assets/images/gradient/05.png"
       alt="circle"
     />
-    <div class="section-inner clear">
+    <div class="section-inner clear" id="gsapContainer">
       <h2 class="sub-title">Project</h2>
       <div
         class="fixed-box"
@@ -18,21 +18,31 @@
       >
         <div class="f-container">
           <ul class="text-list">
-            <li class="on">
+            <li>
               <img src="@/assets/images/project/project_01.png" alt="website" />
-              <h3>Hacker News</h3>
+              <h3><span class="blind">Hacker News</span></h3>
               <p>
-                Vue.js를 사용하여 hacker news 페이지를 구현 하였습니다 api로
-                가져온 데이터는 vuex를 사용하여 관리 및 출력하였으며, 스크롤을
-                최하단 지점에 도달할 경우 api를 호출하여 추가로 데이터를
-                불러오는 무한 스크롤을 적용하였습니다
+                <span class="blind">
+                  Vue.js를 사용하여 hacker news 페이지를 구현 하였습니다 api로
+                  가져온 데이터는 vuex를 사용하여 관리 및 출력하였으며, 스크롤을
+                  최하단 지점에 도달할 경우 api를 호출하여 추가로 데이터를
+                  불러오는 무한 스크롤을 적용하였습니다.
+                </span>
               </p>
               <small>
-                <a href="#" target="_blank" class="github">
+                <a
+                  href="https://github.com/Dong-Yeop/hacker-news"
+                  target="_blank"
+                  class="github blind"
+                >
                   <i></i>
                   <span>GitHub</span>
                 </a>
-                <a href="#" target="_blank" class="link">
+                <a
+                  href="https://dynamic-valkyrie-a6a7f2.netlify.app"
+                  target="_blank"
+                  class="link blind"
+                >
                   <i></i>
                   <span>Page View</span>
                 </a>
@@ -40,20 +50,30 @@
             </li>
             <li>
               <img src="@/assets/images/project/project_02.png" alt="website" />
-              <h3>학습 노트 애플리케이션</h3>
+              <h3><span class="blind">학습 노트 애플리케이션</span></h3>
               <p>
-                로그인하여 게시판 형태로 글을 작성할 수 있는 애플리케이션입니다
-                Vue.js를 사용하여 구현하였으며 인강에서 제공받은 REST API를
-                Swagger 툴을 이용하여 보다 쉽게 API를 이해하고 사용하였습니다
-                API 서버 배포는 heroku를 사용하였고 데이터 베이스는 MongoDB
-                Cloud 서비스를 이용하였습니다.
+                <span class="blind">
+                  로그인하여 게시판 형태로 글을 작성할 수 있는
+                  애플리케이션입니다 Vue.js를 사용하여 구현하였으며 인강에서
+                  제공받은 REST API를 Swagger 툴을 이용하여 보다 쉽게 API를
+                  이해하고 사용하였습니다 API 서버 배포는 heroku를 사용하였고
+                  데이터 베이스는 MongoDB Cloud 서비스를 이용하였습니다.
+                </span>
               </p>
               <small>
-                <a href="#" target="_blank" class="github">
+                <a
+                  href="https://github.com/Dong-Yeop/vue-til"
+                  target="_blank"
+                  class="github blind"
+                >
                   <i></i>
                   <span>GitHub</span>
                 </a>
-                <a href="#" target="_blank" class="link">
+                <a
+                  href="https://vermillion-gecko-f9ddcc.netlify.app"
+                  target="_blank"
+                  class="link blind"
+                >
                   <i></i>
                   <span>Page View</span>
                 </a>
@@ -61,18 +81,16 @@
             </li>
             <li>
               <img src="@/assets/images/project/project_03.png" alt="website" />
-              <h3>대원미디어 리뉴얼</h3>
+              <h3><span class="blind">대원미디어 리뉴얼</span></h3>
               <p>
-                대원미디어의 웹 사이트 리뉴얼 프로젝트의 퍼블리싱 업무를 외주
-                받아 프리랜서로 참여하게 되었습니다 html, css 하드코딩으로 작업
-                진행하였습니다.
+                <span class="blind">
+                  대원미디어의 웹 사이트 리뉴얼 프로젝트의 퍼블리싱 업무를 외주
+                  받아 프리랜서로 참여하게 되었습니다 html, css 하드코딩으로
+                  작업 진행하였습니다.
+                </span>
               </p>
               <small>
-                <a href="#" target="_blank" class="github">
-                  <i></i>
-                  <span>GitHub</span>
-                </a>
-                <a href="#" target="_blank" class="link">
+                <a href="#" target="_blank" class="link blind">
                   <i></i>
                   <span>Page View</span>
                 </a>
@@ -80,86 +98,64 @@
             </li>
             <li>
               <img src="@/assets/images/project/project_04.png" alt="website" />
-              <h3>동아출판</h3>
+              <h3><span class="blind">동아출판</span></h3>
               <p>
-                2020.05 ~ 2022.05 기간 동안 동아출판의 기획자, 디자이너,
-                개발자와 온라인으로 커뮤니케이션 진행하며 동아출판의 각
-                브랜드(티동아, 북동아, 두클래스, 엠동아 등등) 웹 사이트의 유지
-                보수를 진행하였습니다 jsp로 구성된 사이트에 붙어서 html, css,
-                javascript, jquery를 수정 및 보완하였습니다.
+                <span class="blind">
+                  2020.05 ~ 2022.05 기간 동안 동아출판의 기획자, 디자이너,
+                  개발자와 온라인으로 커뮤니케이션 진행하며 동아출판의 각
+                  브랜드(티동아, 북동아, 두클래스, 엠동아 등등) 웹 사이트의 유지
+                  보수를 진행하였습니다 jsp로 구성된 사이트에 붙어서 html, css,
+                  javascript, jquery를 수정 및 보완하였습니다.
+                </span>
               </p>
-              <small>
-                <a href="#" target="_blank" class="github">
-                  <i></i>
-                  <span>GitHub</span>
-                </a>
-                <a href="#" target="_blank" class="link">
-                  <i></i>
-                  <span>Page View</span>
-                </a>
-              </small>
             </li>
             <li>
               <img src="@/assets/images/project/project_05.png" alt="website" />
-              <h3>브이클라우드</h3>
+              <h3><span class="blind">브이클라우드</span></h3>
               <p>
-                브이클라우드는 클라우드 통합 관리 플랫폼으로 기업을 위한 서비스
-                매니지먼트 플랫폼이며, 대시보드 형태의 웹 애플리케이션으로 대략
-                50~60페이지를 하드코딩으로 작업 진행하였습니다
+                <span class="blind">
+                  브이클라우드는 클라우드 통합 관리 플랫폼으로 기업을 위한
+                  서비스 매니지먼트 플랫폼이며, 대시보드 형태의 웹
+                  애플리케이션으로 대략 50~60페이지를 하드코딩으로 작업
+                  진행하였습니다.
+                </span>
               </p>
-              <small>
-                <a href="#" target="_blank" class="github">
-                  <i></i>
-                  <span>GitHub</span>
-                </a>
-                <a href="#" target="_blank" class="link">
-                  <i></i>
-                  <span>Page View</span>
-                </a>
-              </small>
             </li>
           </ul>
-          <strong class="glowing clear"><span>PROJECT</span></strong>
         </div>
+        <strong class="glowing clear"><span>PROJECT</span></strong>
       </div>
-      <div class="img-list" ref="fBox">
-        <img
-          class="website website01 on"
-          src="@/assets/images/project/project_01.png"
-          alt="website"
-        />
-        <img
-          class="website website02"
-          src="@/assets/images/project/project_02.png"
-          alt="website"
-        />
-        <img
-          class="website website03"
-          src="@/assets/images/project/project_03.png"
-          alt="website"
-        />
-        <img
-          class="website website04"
-          src="@/assets/images/project/project_04.png"
-          alt="website"
-        />
-        <img
-          class="website website05"
-          src="@/assets/images/project/project_05.png"
-          alt="website"
-        />
-      </div>
+      <ul class="img-list" ref="fBox">
+        <li class="website">
+          <img src="@/assets/images/project/project_01.png" alt="website" />
+        </li>
+        <li class="website">
+          <img src="@/assets/images/project/project_02.png" alt="website" />
+        </li>
+        <li class="website">
+          <img src="@/assets/images/project/project_03.png" alt="website" />
+        </li>
+        <li class="website">
+          <img src="@/assets/images/project/project_04.png" alt="website" />
+        </li>
+        <li class="website">
+          <img src="@/assets/images/project/project_05.png" alt="website" />
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script>
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
 export default {
   data() {
     return {
       textFixed1: false,
       textFixed2: false,
-      textIndex: ['01', '02', '03', '04', '05'],
     };
   },
   methods: {
@@ -168,44 +164,6 @@ export default {
       let fBoxTop =
         this.$refs.fBox.getBoundingClientRect().top + window.scrollY;
       let boxH = this.$refs.fBox.clientHeight - window.innerHeight;
-      let scrollIndex01 =
-        document.querySelector('.website01').getBoundingClientRect().top +
-        window.scrollY -
-        window.innerHeight / 2;
-      let scrollIndex02 =
-        document.querySelector('.website02').getBoundingClientRect().top +
-        window.scrollY -
-        window.innerHeight / 2;
-      // let scrollIndex03 =
-      //   document.querySelector('.website03').getBoundingClientRect().top +
-      //   window.scrollY -
-      //   window.innerHeight / 2;
-      // let scrollIndex04 =
-      //   document.querySelector('.website04').getBoundingClientRect().top +
-      //   window.scrollY -
-      //   window.innerHeight / 2;
-      // let scrollIndex05 =
-      //   document.querySelector('.website05').getBoundingClientRect().top +
-      //   window.scrollY -
-      //   window.innerHeight / 2;
-
-      console.log(scrollIndex01);
-
-      if (wst > scrollIndex01) {
-        document.querySelector('.img-list').classList.add('on01');
-      } else {
-        document.querySelector('.img-list').classList.remove('on01');
-      }
-      if (wst > scrollIndex02) {
-        document.querySelector('.img-list').classList.add('on02');
-      } else {
-        document.querySelector('.img-list').classList.remove('on01');
-        document.querySelector('.img-list').classList.remove('on02');
-      }
-      // if (wst >= scrollIndex02 - 100 && wst <= scrollIndex02 + 300) {
-      //   document.querySelector('.website').classList.remove('on');
-      //   document.querySelector('.website02').classList.add('on');
-      // }
 
       if (wst >= fBoxTop && wst <= fBoxTop + boxH) {
         // console.log('active');
@@ -220,9 +178,29 @@ export default {
         this.textFixed2 = false;
       }
     },
+    scrollAnimation() {
+      const headings = gsap.utils.toArray('li.website');
+
+      headings.forEach((title, index) => {
+        gsap.to(title, {
+          scrollTrigger: {
+            trigger: title,
+            start: 'top center',
+            end: 'bottom center',
+            scrub: 1,
+            markers: true,
+            toggleClass: {
+              targets: '#gsapContainer',
+              className: 'ani0' + index,
+            },
+          },
+        });
+      });
+    },
   },
   mounted() {
     window.addEventListener('scroll', this.onScroll);
+    this.scrollAnimation();
   },
   beforeDestroy() {
     window.removeEventListener('scroll', this.onScroll);
@@ -267,6 +245,7 @@ export default {
   font-size: 22px;
   font-weight: 500;
   margin: 30px 0 20px;
+  overflow: hidden;
 }
 .text-list p {
   font-size: 14px;
@@ -276,6 +255,7 @@ export default {
 }
 .text-list small {
   display: flex;
+  overflow: hidden;
 }
 .text-list small a {
   display: flex;
@@ -310,12 +290,13 @@ export default {
 /* 가로 해상도 768 이상 */
 @media screen and (min-width: 768px) {
   .section-inner {
-    padding-top: 100px;
+    padding-top: 0;
     padding-bottom: 0;
   }
   .sub-title {
     width: 100%;
     margin-bottom: 0;
+    transform: translateY(100px);
   }
   .gradient01 {
     top: 100px;
@@ -330,20 +311,33 @@ export default {
     float: right;
     width: 50%;
     padding-left: 10px;
-    padding-top: 200px;
-    padding-bottom: 500px;
+    padding-top: 150px;
+    padding-bottom: 200px;
   }
   .img-list img {
     display: block;
     width: 100%;
-    opacity: 0.6;
+    transition: box-shadow 0.5s ease;
+  }
+  .img-list li {
+    padding: 150px 0 200px;
+    opacity: 0.15;
     transition: opacity 0.5s ease;
   }
-  .img-list img.on {
+  #gsapContainer.ani00 .img-list li:nth-child(1),
+  #gsapContainer.ani01 .img-list li:nth-child(2),
+  #gsapContainer.ani02 .img-list li:nth-child(3),
+  #gsapContainer.ani03 .img-list li:nth-child(4),
+  #gsapContainer.ani04 .img-list li:nth-child(5) {
     opacity: 1;
   }
-  .img-list img + img {
-    margin-top: 500px;
+  #gsapContainer.ani00 .img-list li:nth-child(1) img,
+  #gsapContainer.ani01 .img-list li:nth-child(2) img,
+  #gsapContainer.ani02 .img-list li:nth-child(3) img,
+  #gsapContainer.ani03 .img-list li:nth-child(4) img,
+  #gsapContainer.ani04 .img-list li:nth-child(5) img {
+    border-radius: 20px;
+    box-shadow: 0px 0px 20px 10px rgba(255, 150, 0, 0.2);
   }
   .fixed-box {
     float: left;
@@ -369,7 +363,7 @@ export default {
     left: 0;
     transform: translateY(-50%);
     width: 100%;
-    padding: 150px 0;
+    padding-bottom: 100px;
   }
   .text-list {
     padding-right: 10px;
@@ -380,9 +374,6 @@ export default {
   .text-list li {
     display: none;
   }
-  .text-list li.on {
-    display: block;
-  }
   .text-list img {
     display: none;
   }
@@ -390,21 +381,108 @@ export default {
     font-size: 24px;
     margin: 0;
   }
+  .text-list h3 span {
+    display: block;
+  }
   .text-list p {
     font-size: 16px;
     margin: 20px 0 30px;
+    overflow: hidden;
+  }
+  .text-list p span {
+    display: block;
   }
   .glowing {
+    position: absolute;
+    left: 0;
+    bottom: 20%;
     display: block;
-    margin-top: 250px;
     padding-right: 30px;
+    opacity: 0;
+    transition: opacity 1s ease;
+    /* transition-delay: 1s; */
   }
   .glowing span {
     float: right;
     white-space: nowrap;
     font-size: 70px;
-    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #a18b68,
-      0 0 40px #a18b68, 0 0 50px #a18b68, 0 0 60px #a18b68, 0 0 70px #a18b68;
+    text-shadow: 0 0 5px #fff, 0 0 20px #fff, 0 0 30px #a18b68, 0 0 40px #a18b68,
+      0 0 50px #a18b68, 0 0 60px #a18b68, 0 0 70px #a18b68;
+  }
+  #gsapContainer[class*='ani'] .glowing {
+    opacity: 1;
+  }
+
+  #gsapContainer.ani00 .text-list li:nth-child(1),
+  #gsapContainer.ani01 .text-list li:nth-child(2),
+  #gsapContainer.ani02 .text-list li:nth-child(3),
+  #gsapContainer.ani03 .text-list li:nth-child(4),
+  #gsapContainer.ani04 .text-list li:nth-child(5) {
+    display: block;
+  }
+  #gsapContainer.ani00 .text-list li:nth-child(1) .blind,
+  #gsapContainer.ani01 .text-list li:nth-child(2) .blind,
+  #gsapContainer.ani02 .text-list li:nth-child(3) .blind,
+  #gsapContainer.ani03 .text-list li:nth-child(4) .blind,
+  #gsapContainer.ani04 .text-list li:nth-child(5) .blind {
+    animation: text-active-animation 1.3s cubic-bezier(0.4, 0, 0.2, 1) both;
+  }
+
+  @keyframes text-active-animation {
+    from {
+      opacity: 0;
+      -webkit-transform: translateY(50px);
+      transform: translateY(50px);
+    }
+    to {
+      opacity: 1;
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+    }
+  }
+}
+/* 가로 해상도 1024 이상 */
+@media screen and (min-width: 1024px) {
+  .sub-title {
+    transform: translateY(100px);
+  }
+  .img-list {
+    padding-left: 0;
+    width: 400px;
+  }
+  .fixed-box.active1 {
+    width: 492px;
+  }
+  .text-list h3 {
+    font-size: 24px;
+    margin: 0;
+  }
+  .text-list h3 span {
+    display: block;
+  }
+  .text-list p {
+    font-size: 20px;
+    margin: 20px 0 50px;
+  }
+  .glowing {
+    bottom: 18%;
+    padding-right: 0;
+  }
+  .glowing span {
+    float: none;
+    font-size: 80px;
+  }
+}
+/* 가로 해상도 1280 이상 */
+@media screen and (min-width: 1280px) {
+  .sub-title {
+    transform: translateY(100px);
+  }
+  .img-list {
+    width: 500px;
+  }
+  .fixed-box.active1 {
+    width: 600px;
   }
 }
 </style>
